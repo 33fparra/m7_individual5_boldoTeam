fd3429d47576ff1ab9f49abd58472f73baca8225
pipe <felipe.parra@blackmind.cl>
2023-08-18 21:20:32 -0400

    listo el job

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize('individual5', 'postgres', 'pipe1234', {
 });
 
 // aca esta el modelito del rectangulo
-const Rectangulo = sequelize.define('Rectangulo', {
+const RectanguloModel = sequelize.define('Rectangulo', {
   ladox: {
     type: DataTypes.INTEGER,
     allowNull: false
@@ -18,8 +18,94 @@ const Rectangulo = sequelize.define('Rectangulo', {
   }
 });
 
-// Sincronización del modelo con la base de datos  esto esta raro y
-Rectangulo.sync({ force: true })
-  .then(() => console.log('Tabla Rectangulo creada')) 
-  .catch(error => console.error('Error al crear la tabla Rectangulo:', error))
-  .finally(() => sequelize.close());
+const TrianguloModel = sequelize.define('Triangulo', {
+  ladox: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  ladoy: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  ladoz: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+
+const CirculoModel = sequelize.define('Circulo', {
+  Radior: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+});
+
+
+
+//las clases
+class Rectangulo {
+  constructor(x, y) {
+  this.x = x;
+  this.y = y
+  }
+  calcularArea() {
+  return this.x * this.y;
+  }
+  calcularPerimetro() {
+  return (this.x + this.y) * 2;
+  }
+}
+  
+class Triangulo {
+    constructor(x, y) {
+    this.x = x;
+    this.y = y
+  }
+  calcularArea() {
+  return (this.x * this.y) / 2;
+  }
+  calcularPerimetro() {
+  let z = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+  return (this.x + this.y + z);
+  }
+}
+  
+class Circulo {
+    constructor(r) {
+    this.r = r;
+    }
+    calcularArea() {
+    return this.r * this.r * Math.PI;
+    }
+    calcularPerimetro() {
+    return this.r * Math.PI * 2;
+    }
+}
+
+// RectanguloModel.sync().then(() => {
+//   console.log('nueva tabla Rectangulo creada');
+//   }).finally(() => {
+//   sequelize.close();
+//   })
+// TrianguloModel.sync().then(() => {
+//   console.log('nueva tabla Triangulo creada');
+//   }).finally(() => {
+//   sequelize.close();
+//   })
+// CirculoModel.sync().then(() => {
+//   console.log('nueva tabla Circulo creada');
+//   }).finally(() => {
+//   sequelize.close();
+//   })
+
+
+Promise.all([
+  RectanguloModel.sync(),
+  TrianguloModel.sync(),
+  CirculoModel.sync()
+]).then(() => {
+  console.log('Tabla Rectangulo creada');
+  console.log('Tabla Triangulo creada');
+  console.log('Tabla Circulo creada');
+  sequelize.close();
+});
